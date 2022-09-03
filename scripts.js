30596097ea6644991a443579cef8198c2a3653bf
Liedson Barros <liedson.b9@gmail.com>
2022-09-03 11:58:58 -0300

    mudanças na interface

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const menuBtn = document.getElementById("menu");
 const screen = document.getElementById("screen-main");
 const loginClose = document.getElementById("close-login");
 const loginForm = document.getElementById("container-login");
+const nav = document.getElementsByClassName("nav");
 
 menuBtn.addEventListener('click', () => {
     loginForm.classList.toggle('hide');
